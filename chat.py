47fb84187535d00181395031a9e88d677111090e
Sunil Mathew <sunil.pulinthitta@gmail.com>
2023-01-23 16:09:41 -0600

    Update chat.py
    Adding prompt history and font size update.

--- chat.py
+++ chat.py
@@ -1,10 +1,11 @@
 # Author: Sunil Mathew
-# Date: 17 Jan 2023
-
+# Created date: 17 Jan 2023
+import os
 import openai
 
-from qtpy import uic
-from qtpy.QtWidgets import QApplication, QMainWindow, QWidget
+from qtpy import uic, QtGui
+from qtpy.QtWidgets import QApplication, QMainWindow, QTreeWidget, QWidget, QTreeWidgetItem
+from qtpy.QtGui import QStandardItemModel
 
 class Ui(QMainWindow):
 
@@ -21,19 +22,67 @@ class Ui(QMainWindow):
         widget.setLayout(self.hLytMain)
         self.setCentralWidget(widget)
 
+        self.fontsize_update()
+        self.init_history_view()
         self.init_openai()
         self.register_events()
 
+    def init_ui_font(self):
+        font = QtGui.QFont()
+        font.setPointSize(20)
+
+        self.txtInput.setFont(font)
+        self.txtOutput.setFont(font)
+
     def register_events(self):
+        self.dlFontSize.valueChanged.connect(self.fontsize_update)
         self.btnSubmit.clicked.connect(self.gen_results)    
     
     #endregion Initialization
 
+    #region UI
+
+    def init_history_view(self):
+        self.prompt_count = 0
+        self.history = {}
+
+        #adding the tree widget to a frame
+        self.frameHistory.setLayout(self.vLytHistory)
+        self.treeHistory.clicked.connect(self.treeWidgetClicked)
+        self.treeHistory.setHeaderLabels(["History"])
+
+        self.chkHistory.toggled.connect(self.show_hide_history)
+    
+    def show_hide_history(self):
+        if self.chkHistory.isChecked():
+            self.frameHistory.show()
+        else:
+            self.frameHistory.hide()
+
+    def treeWidgetClicked(self, qModelIndex):
+        item = self.treeHistory.currentItem()
+        txtInput = list(self.history[item.text(0)].keys())[0]
+        txtOutput = list(self.history[item.text(0)].values())[0]
+        self.txtInput.setPlainText(txtInput)
+        self.txtOutput.setHtml(txtOutput)
+
+    def fontsize_update(self):
+        font_sz = self.dlFontSize.value()
+        self.lblFontSize.setText("Font size: " + str(font_sz))
+
+        font = QtGui.QFont()
+        font.setPointSize(font_sz)
+
+        self.txtInput.setFont(font)
+        self.txtOutput.setFont(font)
+
+    #endregion UI
+
     #region OpenAI
 
     def init_openai(self):
-        # openai.api_key = os.getenv("OPENAI_API_KEY")
-        openai.api_key_path = 'chat.env'
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        # openai.api_key_path = 'chat.env'
 
     def gen_results(self):
         text_input = self.txtInput.toPlainText()
@@ -53,8 +102,15 @@ class Ui(QMainWindow):
         )
         res = response['choices'][0]['text']
         self.txtOutput.clear()
-        self.txtOutput.setPlainText(res)
-        # print(ch)
+        txtOutput = '<span style="white-space: pre;"><code>'+res+'</code></span>'
+        self.txtOutput.setHtml(txtOutput)
+        # print(res)
+
+        self.prompt_count += 1
+        hist_item = QTreeWidgetItem(self.treeHistory)
+        key = 'Prompt ' + str(self.prompt_count)
+        hist_item.setText(0, key)
+        self.history[key] = {text_input : txtOutput}
 
     #endregion OpenAI
 
